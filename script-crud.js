d4f547f12f6693a2ea56d39e3b8740bf651996f3
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-22 00:50:07 -0300

    Displaying the form

---
+++ script-crud.js
@@ -0,0 +1,7 @@
+// encontrar o botão adicionar tarefa
+const btnAddTask = document.querySelector('.app__button--add-task')
+const formAddTask = document.querySelector('.app__form-add-task')
+
+btnAddTask.addEventListener('click', () => {
+    formAddTask.classList.toggle('hidden')
+})
