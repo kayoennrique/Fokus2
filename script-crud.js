ba7a5de5c9f8a071a35354523169a18100c82737
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-22 01:02:53 -0300

    Submitting form and consuming a json API

--- script-crud.js
+++ script-crud.js
@@ -1,7 +1,18 @@
-// encontrar o botão adicionar tarefa
 const btnAddTask = document.querySelector('.app__button--add-task')
 const formAddTask = document.querySelector('.app__form-add-task')
+const textarea = document.querySelector('.app__form-textarea')
+
+const tasks = []
 
 btnAddTask.addEventListener('click', () => {
     formAddTask.classList.toggle('hidden')
+})
+
+formAddTask.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const task = {
+        description: textarea.value
+    }
+    tasks.push(task)
+    localStorage.setItem('tasks', JSON.stringify(tasks))
 })
