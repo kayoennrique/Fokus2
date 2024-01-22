acb6b5f61819ec1ae2a48659077542d8bd27c3b0
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-22 01:10:44 -0300

    Create task

--- script-crud.js
+++ script-crud.js
@@ -1,18 +1,46 @@
-const btnAddTask = document.querySelector('.app__button--add-task')
-const formAddTask = document.querySelector('.app__form-add-task')
-const textarea = document.querySelector('.app__form-textarea')
+const btnAddTask = document.querySelector('.app__button--add-task');
+const formAddTask = document.querySelector('.app__form-add-task');
+const textarea = document.querySelector('.app__form-textarea');
 
-const tasks = []
+const tasks = [];
+
+function createTaskElement(task) {
+    const li = document.createElement('li');
+    li.classList.add('app__section-task-list-item');
+
+    const svg = document.createElement('svg');
+    svg.innerHTML = `
+        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z"
+                fill="#01080E"></path>
+        </svg>
+    `;
+
+    const paragraph = document.createElement('p');
+    paragraph.textContent = task.description
+
+    const button = document.createElement('button');
+    const buttonImage = document.createElement('img');
+    buttonImage.setAttribute('src', '/images/edit.png');
+
+    button.append(buttonImage);
+
+    li.append(svg)
+    li.append(paragraph)
+    li.append(button)
+}
 
 btnAddTask.addEventListener('click', () => {
     formAddTask.classList.toggle('hidden')
-})
+});
 
 formAddTask.addEventListener('submit', (e) => {
     e.preventDefault();
     const task = {
         description: textarea.value
-    }
-    tasks.push(task)
-    localStorage.setItem('tasks', JSON.stringify(tasks))
-})
+    };
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+});
